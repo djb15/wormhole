ab1c845c323f14b5269d1d50e11ff128f095be76
Dirk Brink <hello@dirk.tech>
2025-05-30 16:29:52 -0700

    test

--- node/pkg/governor/manual_tokens.go
+++ node/pkg/governor/manual_tokens.go
@@ -1,5 +1,5 @@
 package governor
-
+// test
 // manualTokenList() returns a list of mainnet tokens that are added manually because they cannot be auto generated.
 func manualTokenList() []tokenConfigEntry {
 	return []tokenConfigEntry{
